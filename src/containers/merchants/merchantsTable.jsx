299d826323f518378980840b065603165af8d807
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2024-01-21 21:12:12 +0100

    added merchant general tab details

--- src/containers/merchants/merchantsTable.jsx
+++ src/containers/merchants/merchantsTable.jsx
@@ -3,11 +3,13 @@ import { merchantData } from "../../data/merchant";
 import { optionIcon } from "../../constants/defaultValues";
 import BigModalLayout from "../../layout/BigModalLayout";
 import "../../assets/css/merchant.css";
+import { merchantGeneralData } from "../../data/merchant_general_data";
 
 function MerchantsTable() {
   const [clickedMenu, setClickedMenu] = useState("0");
   const [showModal, setShowModal] = useState(false);
   const [activeBtn, setActiveBtn] = useState(0);
+  const [showOptionMenu, setShowOptionMenu] = useState(false);
   const btnList = [
     {
       id: 0,
@@ -130,13 +132,105 @@ function MerchantsTable() {
               <button
                 onClick={() => setActiveBtn(index)}
                 className={`merchant_nav_btn ${
-                  activeBtn === index ? "merchant_nav_activebtn" : "merchant_nav_notactivebtn"
+                  activeBtn === index
+                    ? "merchant_nav_activebtn"
+                    : "merchant_nav_notactivebtn"
                 }`}
               >
                 {item.btn_name}
               </button>
             ))}
           </div>
+          {activeBtn === 0 && <div className='merchant_general'>
+            <div className='merchant_general_left'>
+              <div className='merchant_general_profileimg'>
+                <span>BL</span>
+              </div>
+              <label className='merchant_edit_profile'>Edit Profile</label>
+
+              <div className='merchant_business_profile'>
+                <p className='merchant_bus_name'>Business Name</p>
+                <p className='merchant_bus_type'>Business Type</p>
+                <div className='merchant_bus_status'>
+                  <span>Active</span>
+                </div>
+              </div>
+            </div>
+            <div className='merchant_general_right'>
+              <table className='service-details-table'>
+                {merchantGeneralData.map((item, index) => (
+                  <div key={index}>
+                    <tr>
+                      <th>Merchant ID</th>
+                      <td>{item.merchant_id}</td>
+                    </tr>
+                    <tr>
+                      <th>Wallet Balance</th>
+                      <td>{item.wallet_balance}</td>
+                    </tr>
+                    <tr>
+                      <th>Email Address</th>
+                      <td>{item.email_address}</td>
+                    </tr>
+                    <tr>
+                      <th>Phone Number</th>
+                      <td>{item.phone_number}</td>
+                    </tr>
+                    <tr>
+                      <th>Address</th>
+                      <td>{item.address}</td>
+                    </tr>
+                    <tr>
+                      <th>Opening Days</th>
+                      <td>{item.opening_days}</td>
+                    </tr>
+                    <tr>
+                      <th>Opening Hours</th>
+                      <td>{item.opening_hours}</td>
+                    </tr>
+                    <tr>
+                      <th>Subscription Plan</th>
+                      <td>{item.subscription_plan}</td>
+                    </tr>
+                    <tr>
+                      <th>Date Created</th>
+                      <td>{item.date_created}</td>
+                    </tr>
+                    <tr>
+                      <th>Last Login Date</th>
+                      <td>{item.last_login_date}</td>
+                    </tr>
+                    <tr>
+                      <th>Actions</th>
+                      <td className='optionbtn-modal'>
+                        <div
+                          className='optionbtn'
+                          onClick={() => setShowOptionMenu(true)}
+                        >
+                          {optionIcon}
+                        </div>
+                        {showOptionMenu && (
+                          <>
+                            <div
+                              className='optionmenu-overlay'
+                              onClick={() => setShowOptionMenu(false)}
+                            />
+                            <div className='optionmenu-body'>
+                              <p onClick={() => {}} className='optionmenu-item'>
+                                Edit
+                              </p>
+                              <p className='optionmenu-item'>Suspend</p>
+                              <p className='optionmenu-item'>Delete</p>
+                            </div>
+                          </>
+                        )}
+                      </td>
+                    </tr>
+                  </div>
+                ))}
+              </table>
+            </div>
+          </div>}
         </BigModalLayout>
       )}
     </div>
---
+++ src/data/merchant_general_data.js
@@ -0,0 +1,15 @@
+export const merchantGeneralData = [
+  {
+    id: 0,
+    merchant_id: "Merchant ID",
+    wallet_balance: "Wallet Balance",
+    email_address: "Email Address",
+    phone_number: "Phone Number",
+    address: "Address can be 2 lines long.",
+    opening_days: "Opening Days",
+    opening_hours: "9am - 9pm (12 hours)",
+    subscription_plan: "Subscription Plan",
+    date_created: "Date Created",
+    last_login_date: "Last Login Date",
+  },
+];
